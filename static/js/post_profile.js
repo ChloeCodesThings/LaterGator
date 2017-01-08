d852dc0db1aaa5118b152b256deb6e799d705352
Chloe Condon <misschloecondon@gmail.com>
2017-01-08 00:18:52 +0000

    cleaning up JS some more

--- static/js/post_profile.js
+++ static/js/post_profile.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $("#userpost").on('input', function() {
-        if ($(this).val().length>=140) {
+        if ($(this).val().length>=63206) {
             alert('Oops! Remember- Facebook has a 63,206 character limit!');
          }
          
@@ -14,17 +14,11 @@ $(document).ready(function(){
     $('#time_right_now').html(current_time);
 
     function changedTime() {
-         // time_from_textbox is whatever they typed in
         var time_from_textbox = $('#time_textbox').val();
-        console.log('time_from_textbox', time_from_textbox);
 
-        // Parsed chrono date (chrono.parseDate(x))
         var chrono_time = chrono.parseDate(time_from_textbox);
-        console.log('chrono_time', chrono_time);
 
-        // format that to the value we want to actually send to facebook (seconds epoch value)
         var moment_time = moment(chrono_time);
-        console.log('moment_time', moment_time);
 
         var moment_unix = moment_time.format('X');
 
@@ -34,35 +28,20 @@ $(document).ready(function(){
 
         var value_check = moment_unix - current_time_unix;
 
-        console.log(value_check);
-
-        console.log(timezone);
-        console.log(moment_unix);
-        console.log(current_time_unix);
-
         if (value_check <= 600) {
             alert("You must post at least 10 minutes from now!");
         } else if (value_check >= 5184000) {
             alert("You cannot post more than 60 days ahead of time!");
         }
 
-        // setting our final time value (seconds since the epoch) to our hidden input
         $('#hidden_time').val(moment_unix);
         $('#time_example').html(chrono_time);
 
     }
     $('#time_textbox').on('input', changedTime);
     var current_time = moment();
-    console.log(current_time);
-
-
-    // if (value_check <= 600) {
-    //     alert("NOPE!");
-    // }
 
     var hour_of_current_time = parseInt(current_time.format('H'));
-    console.log(hour_of_current_time);
-
 
     if (hour_of_current_time < 8) {
         $('#time_suggestion').html("Today at 8am");
--- static/js/post_twitter.js
+++ static/js/post_twitter.js
@@ -19,17 +19,12 @@ $(document).ready(function(){
     $('#time_right_now').html(current_time);
 
     function changedTime() {
-         // time_from_textbox is whatever they typed in
+
         var time_from_textbox = $('#time_textbox').val();
-        console.log('time_from_textbox', time_from_textbox);
 
-        // Parsed chrono date (chrono.parseDate(x))
         var chrono_time = chrono.parseDate(time_from_textbox);
-        console.log('chrono_time', chrono_time);
 
-        // format that to the value we want to actually send to facebook (seconds epoch value)
         var moment_time = moment(chrono_time);
-        console.log('moment_time', moment_time);
 
         var moment_unix = moment_time.format('X');
 
@@ -39,26 +34,12 @@ $(document).ready(function(){
 
         var value_check = moment_unix - current_time_unix;
 
-        console.log(value_check);
-
-        console.log(timezone);
-        console.log(moment_unix);
-        console.log(current_time_unix);
-
         if (value_check <= 600) {
             alert("You must post at least 10 minutes from now!");
         } else if (value_check >= 5184000) {
             alert("You cannot post more than 60 days ahead of time!");
         }
 
-        console.log(timezone);
-        console.log(moment_unix);
-
-        console.log(timezone);
-        console.log(moment_unix);
-
-
-        // setting our final time value (seconds since the epoch) to our hidden input
         $('#hidden_time').val(moment_unix);
         $('#time_example').html(chrono_time);
 
@@ -67,9 +48,6 @@ $(document).ready(function(){
     var current_time = moment();
 
     var hour_of_current_time = parseInt(current_time.format('H'));
-     var hour_of_current_time = parseInt(current_time.format('H'));
-    console.log(hour_of_current_time);
-
 
     if (hour_of_current_time < 8) {
         $('#time_suggestion').html("Today at 8am");
